2182bd38d0be41c89c4006199be3dd159d6bf4c2
Sean Redmond <github-smr@sneakemail.com>
2017-04-28 23:00:14 -0400

    Improve combinations
    Use only unique, unordered combinations.

--- lib/a11y/color/matrix/cli.rb
+++ lib/a11y/color/matrix/cli.rb
@@ -1,18 +1,15 @@
 module A11y
   module Color
     module Matrix
-
       class CLI < Thor
         desc "check", "check list of colors"
         def check(*c)
           colors = Set.new c
-          colors.sort.each do |bg|
-            colors.sort.each do |fg|
-              ratio = WCAGColorContrast.ratio(fg.dup, bg.dup) 
-              if fg != bg
-                puts "#{fg}/#{bg}: #{ratio}"
-                puts 
-              end
+          colors.sort.combination(2).each do |fg, bg|
+            ratio = WCAGColorContrast.ratio(fg.dup, bg.dup) 
+            if fg != bg
+              puts "#{fg}/#{bg}: #{ratio}"
+              puts 
             end
           end
         end
