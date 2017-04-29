ff26d8a2e5ab7a9af6827c4afae6b5a1e15e4266
Sean Redmond <github-smr@sneakemail.com>
2017-04-28 23:20:57 -0400

    Add b/w options. Improve output format

--- lib/a11y/color/matrix/cli.rb
+++ lib/a11y/color/matrix/cli.rb
@@ -2,15 +2,34 @@ module A11y
   module Color
     module Matrix
       class CLI < Thor
+        option :w, :type => :boolean, :desc => "add white to comparisons"
+        option :b, :type => :boolean, :desc => "add black to comparision"
         desc "check", "check list of colors"
         def check(*c)
-          colors = Set.new c
+          colors = Set.new add_white(add_black(c, options['b']), options['w'])
           colors.sort.combination(2).each do |fg, bg|
             ratio = WCAGColorContrast.ratio(fg.dup, bg.dup) 
             if fg != bg
-              puts "#{fg}/#{bg}: #{ratio}"
-              puts 
+              puts "% 6s/% 6s: %5.2f" % [fg, bg, ratio]
             end
+          end
+        end
+
+        no_commands do
+          def add_black(c, b)
+            if b
+              return c + ['000000']
+            end
+            
+            return c
+          end
+          
+          def add_white(c, w)
+            if w
+              return c + ['ffffff']
+            end
+
+            return c
           end
         end
       end
